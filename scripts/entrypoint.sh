bb5aac1894006b31cfa20f2d177e3895708b179d
Jordan Miller <jmiller182@dendrite.ecn.uiowa.edu>
2026-04-15 16:35:41 -0500

    changed around arguemnt order

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 4 ]; then
     echo "Error: Not enough arguments to run."
-    echo "Usage: docker run cornn_local [/path/to/.nii/file] [/cornn/out/path] [num_threads]"
+    echo "Usage: docker run cornn_local [/path/to/.nii/file] [/cornn/out/path] [/slant/out/dir] [/wml/out/dir] [num_threads]"
     exit 1
 fi
 
 NII_PATH=$1
 CORNN_OUT=$2
-THREADS=${3:-1}
+SLANT_OUT=$3
+WML_OUT=$4
+THREADS=${5:-1}
 
-/app/scripts/run.sh "$NII_PATH" "$CORNN_OUT" --num_threads "$THREADS"
+/app/scripts/run.sh "$NII_PATH" "$CORNN_OUT" "--slant $SLANT_OUT" "--wml $WML_OUT" --num_threads "$THREADS"
